002bbc0d2f94dd179ea54f712a35e7dbdbea3157
smueksch <sebastian.mueksch+github@gmail.com>
2020-02-22 00:37:59 +0000

    Adding specialised classes for connection request and data channels.

--- src/common/shared_memory.cpp
+++ src/common/shared_memory.cpp
@@ -1,5 +1,9 @@
 #include "shared_memory.hpp"
 
+/***********
+ * Channel *
+ ***********/
+
 Channel::Channel() : name(""), size(0), descriptor(-1) {
 }
 
@@ -9,14 +13,20 @@ void Channel::create(std::string channel_name,
     int fd = shm_open(channel_name.c_str(), O_CREAT | flags, 0666);
 
     if (fd < 0) {
-        perror("Error opening connection request channel:");
+        perror("Error opening channel:");
         exit(EXIT_FAILURE);
     }
 
     if (ftruncate(fd, channel_size) < 0) {
-        perror("Error setting size of connection request channel:");
+        perror("Error setting size of channel:");
         exit(EXIT_FAILURE);
     }
+
+    map_to_memory();
+
+    if ((shared_mem_ptr = mmap (NULL, sizeof (struct shared_memory), PROT_READ | PROT_WRITE, MAP_SHARED,
+            fd_shm, 0)) == MAP_FAILED)
+       error ("mmap");
 
     this.name = channel_name;
     this.size = size;
@@ -27,14 +37,16 @@ void Channel::open(std) {
     int fd = shm_open(channel_name.c_str(), O_CREAT | flags, 0666);
 
     if (fd < 0) {
-        perror("Error opening connection request channel:");
+        perror("Error opening channel:");
         exit(EXIT_FAILURE);
     }
 
     if (ftruncate(fd, channel_size) < 0) {
-        perror("Error setting size of connection request channel:");
+        perror("Error setting size of channel:");
         exit(EXIT_FAILURE);
     }
+
+    map_to_memory();
 
     this.name = channel_name;
     this.descriptor = fd;
@@ -46,4 +58,77 @@ void Channel::close() {
 
 std::string Channel::get_name() {
     return this.name;
+}
+
+/****************************
+ * ConnectionRequestChannel *
+ ****************************/
+
+void ConnectionRequestChannel::create(std::string channel_name,
+                                      Channel::Flags flags) {
+    base::create(channel_name, CONNECTION_REQUEST_CHANNEL_SIZE, flags);
+}
+
+int ConnectionRequestChannel::read() {
+    return this.data[START_INDEX];
+}
+
+void ConnectionRequestChannel::write(int value) {
+    this.data[START_INDEX] = value;
+}
+
+void ConnectionRequestChannel::map_to_memory() {
+    this.data = (int*)mmap(NULL,
+                           CONNECTION_REQUEST_CHANNEL_SIZE,
+                           PROT_READ | PROT_WRITE,
+                           MAP_SHARED,
+                           this.descriptor,
+                           0);
+
+    if (this.data == MAP_FAILED) {
+        perror("Error mapping connection requrest into memory:");
+    }
+}
+
+/***************
+ * DataChannel *
+ ***************/
+
+void DataChannel::create(std::string channel_name,
+                         Channel::Flags flags) {
+    base::create(channel_name, DATA_CHANNEL_SIZE, flags);
+}
+
+Data DataChannel::read() {
+    Data output;
+
+    output.operation_flag = this.operation_flag[START_INDEX];
+    output.bucket_id = this.bucket_id[START_INDEX];
+    memcpy(output.data, this.data, NUM_DATA_CHANNEL_DATA_ENTRIES);
+
+    return output;
+}
+
+void DataChannel::write(Data value) {
+    this.operation_flag[START_INDEX] = value.operation_flag;
+    this.bucket_id[START_INDEX] = value.bucket_id;
+    memcpy(this.data, value.data, NUM_DATA_CHANNEL_DATA_ENTRIES);
+}
+
+void DataChannel::map_to_memory() {
+    void* mem_ptr = (int*)mmap(NULL,
+                               DATA_CHANNEL_SIZE,
+                               PROT_READ | PROT_WRITE,
+                               MAP_SHARED,
+                               this.descriptor,
+                               0);
+
+    if (mem_ptr == MAP_FAILED) {
+        perror("Error mapping connection requrest into memory:");
+    }
+
+    // Set the pointers according to the memory layout within the channel.
+    this.operation_flag = (dc_flag_t*)mem_ptr;
+    this.bucket_id = (dc_id_t*)(mem_ptr + sizeof(dc_flag_t));
+    this.data = (dc_data_t*)(mem_ptr + sizeof(dc_flag_t) + sizeof(dc_data_t));
 }
--- src/common/shared_memory.hpp
+++ src/common/shared_memory.hpp
@@ -25,6 +25,29 @@ constexpr char CONNECTION_REQUEST_CHANNEL_NAME[] = "connection-requests";
 // which the server interacts with the client in question.
 constexpr off_t CONNECTION_REQUEST_CHANNEL_SIZE = sizeof(int);
 
+// Offset to start reading from in channels.
+constexpr int START_INDEX = 0;
+
+// Number of characters that a DataChannel can hold.
+constexpr int NUM_DATA_CHANNEL_DATA_ENTRIES = 59;
+
+// Typedefs for DataChannel types.
+// Allows for calculations relating to size of DataChannel fields to be
+// easier maintainable.
+typedef char dc_flag_t;
+typedef int dc_id_t;
+typedef char dc_data_t;
+
+// Size of the data channel.
+//
+// Calculation: 1 byte for flag + Size of ID in byte + 1 byte for each data
+// entry possible in a data channel.
+//
+// In theory, this should be 64 Byte (chosen arbitrarily).
+constexpr int DATA_CHANNEL_SIZE = sizeof(dc_flag_t) + 
+                                  sizeof(dc_id_t) +
+                                  NUM_DATA_CHANNEL_DATA_ENTRIES * sizeof(dc_data_t);
+
 /***********
  * Classes *
  ***********/
@@ -50,10 +73,55 @@ class Channel {
 
         std::string get_name();
 
-    private:
+    protected:
+        // Map the shared memory file into memory.
+        virtual void map_to_memory();
+
         std::string name;
         off_t size;
         file_t descriptor;
+};
+
+// Channel which is used to establish a connection between the server and the
+// client.
+class ConnectionRequestChannel : Channel {
+    public:
+        // Overwrite function with predetermined size.
+        void create(std::string channel_name,
+                    Channel::Flags flags);
+
+        int read();
+        void write(int value);
+
+    private:
+        int* data;
+
+        void map_to_memory();
+};
+
+// Channel representing connection between server and client. Both server and
+// client will have one instance of these.
+class DataChannel : Channel {
+    public:
+        struct Data {
+            dc_flag_t operation_flag;
+            dc_id_t bucket_id;
+            dc_data_t data[NUM_DATA_CHANNEL_DATA_ENTRIES];
+        };
+
+        // Overwrite function with predetermined size.
+        void create(std::string channel_name,
+                    Channel::Flags flags);
+
+        DataChannel::Data read();
+        void write(DataChannel::Data value);
+
+    private:
+        dc_flag_t* operation_flag;
+        dc_id_t* bucket_id;
+        dc_data_t* data;
+
+        void map_to_memory();
 };
 
 #endif
